81d358f47122f6a96c717e6ee58224c23c2f47ff
Tim M <timothy.romero.manas@gmail.com>
2024-02-27 21:34:11 -0500

    Create API Endpoint to fetch all products

--- server/controllers/productControllers.js
+++ server/controllers/productControllers.js
@@ -1,5 +1,13 @@
-const getAllProducts = (req, res) => {
-  res.send('Get All products')
+import Product from '../models/Products.js'
+
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find({})
+    console.log(products)
+    res.send(products)
+  } catch (err) {
+    res.statudCode(500).send('Error Unable to find products')
+  }
 }
 
 const getProduct = (req, res) => {
--- server/data/products.js
+++ server/data/products.js
@@ -1,65 +1,71 @@
 const products = [
   {
-    name: 'Airpods Wireless Bluetooth Headphones',
-    image: '/images/airpods.jpg',
+    name: 'Prodct ABC Watch',
+    image:
+      'https://images.unsplash.com/photo-1523275335684-37898b6baf30?q=80&w=2599&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     description:
       'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
-    brand: 'Apple',
+    brand: 'ABC',
     category: 'Electronics',
     price: 89.99,
     url: 'https://www.apple.com',
     countInStock: 5,
   },
   {
-    name: 'iPhone 13 Pro 256GB Memory',
-    image: '/images/phone.jpg',
+    name: 'Prodct 123',
+    image:
+      'https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     description:
-      'Introducing the iPhone 13 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
-    brand: 'Apple',
+      'Introducing the Camero Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+    brand: '123',
     category: 'Electronics',
     price: 599.99,
     url: 'https://www.apple.com',
     countInStock: 2,
   },
   {
-    name: 'Cannon EOS 80D DSLR Camera',
-    image: '/images/camera.jpg',
+    name: 'Prodct CD Watch',
+    image:
+      'https://images.unsplash.com/photo-1549482199-bc1ca6f58502?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     description:
-      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
-    brand: 'Cannon',
+      'Characterized by versatile imaging specs, the Watch EOS  further clarifies itself using a pair of robust focusing systems and an intuitive design',
+    brand: 'CD',
     category: 'Electronics',
     price: 929.99,
     url: 'https://www.cannon.com',
     countInStock: 3,
   },
   {
-    name: 'Sony Playstation 5',
-    image: '/images/playstation.jpg',
+    name: 'Prodct XYZ EarPhones',
+    image:
+      'https://images.unsplash.com/photo-1585565804112-f201f68c48b4?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     description:
-      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
-    brand: 'Sony',
+      'The ultimate home entertainment center starts with XYZ. Whether you are into gaming, HD movies, television, music',
+    brand: 'XYZ',
     category: 'Electronics',
     price: 399.99,
     url: 'https://www.sony.com',
     countInStock: 1,
   },
   {
-    name: 'Logitech G-Series Gaming Mouse',
-    image: '/images/mouse.jpg',
+    name: 'Prodct LMO Watch',
+    image:
+      'https://images.unsplash.com/photo-1616169466279-ed151c4f2174?q=80&w=2482&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     description:
-      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
-    brand: 'Logitech',
+      'Get a better handle on your games with this LMO WATCH LIGHTSYNC. The six programmable buttons allow customization for a smooth playing experience',
+    brand: 'LMO',
     category: 'Electronics',
     price: 49.99,
     url: 'https://www.Logitech.com',
     countInStock: 0,
   },
   {
-    name: 'Amazon Echo Dot 3rd Generation',
-    image: '/images/alexa.jpg',
+    name: 'Prodct LOL Cup',
+    image:
+      'https://images.unsplash.com/photo-1514228742587-6b1558fcca3d?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     description:
-      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
-    brand: 'Amazon',
+      'Meet Echo Cup - Our most popular smart cup with a fabric design. It is our most compact smart cup that fits perfectly into small space',
+    brand: 'LOL',
     category: 'Electronics',
     price: 29.99,
     url: 'https://www.amazon.com',
