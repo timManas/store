488badc0c2eba55e03717fd2b41c0d7a8bc7739d
Tim M <timothy.romero.manas@gmail.com>
2024-02-27 21:40:41 -0500

    Adding endpoint to fetch single product

--- server/controllers/productControllers.js
+++ server/controllers/productControllers.js
@@ -6,12 +6,20 @@ const getAllProducts = async (req, res) => {
     console.log(products)
     res.send(products)
   } catch (err) {
-    res.statudCode(500).send('Error Unable to find products')
+    res.status(500).send('Error Unable to find products')
   }
 }
 
-const getProduct = (req, res) => {
-  res.send(`Get product ${req.params.id}`)
+const getProduct = async (req, res) => {
+  console.log('Product id: ' + req.params.id)
+  try {
+    const product = await Product.findOne({ _id: req.params.id })
+    res.send(product)
+  } catch (err) {
+    res
+      .status(500)
+      .send('Error Unable to find product with id: ' + req.params.id)
+  }
 }
 
 export { getAllProducts, getProduct }
